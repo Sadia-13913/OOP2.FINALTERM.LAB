57cf2a27bf38856b5fffea11ba38482ebadc8c40
Sadia <72723291+Sadia-13913@users.noreply.github.com>
2020-11-27 15:40:33 +0600

    Test Interface 1
    Test Interface 1

---
+++ Final Lab 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Final_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(" Sum = " + c.sum(10,5));
+            Console.WriteLine(" Sub = " + c.sub(10,5));
+            Console.WriteLine(" Multiplication = " + c.multiplication(50,55));
+            Console.WriteLine(" Division = " + c.division(200,55));
+            Console.WriteLine(" Power = " + c.XtoY(60,59));
+            Console.WriteLine(" Factorial = " + c.factorial(8));
+        }
+    }
+}
